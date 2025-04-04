423a84b3ee4d73f1850cf8b169b6b069677939f7
Gabriel Carvalho <gabriellcartads2024@gmail.com>
2025-04-04 14:57:12 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -27,3 +27,27 @@ def criar_tabela_clientes():
     conexao.commit()
     cursor.close()
     conexao.close()
+    
+def cadastrar_cliente(nome,idade,telefone,email,sexo):
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+        cursor.execute ("""
+                           INSERT INTO clientes (nome, idade, telefone,email,sexo)
+                           VALUES (?,?,?,?,?)
+                           
+                        """,(nome,idade,telefone,email,sexo))
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+        
+def listar_clientes():
+        conexao = conectar()
+        cursor = conexao.cursor()
+        
+        cursor.execute("SELECT * FROM clientes")
+        clientes = cursor.fetchall()
+        cursor.close()
+        conexao.close()
+        return clientes
+    
