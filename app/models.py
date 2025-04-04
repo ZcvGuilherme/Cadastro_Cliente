2c688dcf1dd0379ccfea21a2b341de6e58039321
Gabriel Carvalho <gabriellcartads2024@gmail.com>
2025-04-04 14:31:29 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -14,33 +14,16 @@ def criar_tabela_clientes():
 
 
     cursor.execute("""
-               CREATE TABLE IF NOT EXISTS clientes (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                    nome TEXT  NOT NULL, 
-                    idade INTEGER NOT NULL,
-                    telefone TEXT,
-                    email TEXT,
-                    sexo TEXT (1) CHECK (sexo IN ('M','F'))
+            CREATE TABLE IF NOT EXISTS clientes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                nome TEXT NOT NULL, 
+                idade INTEGER NOT NULL,
+                telefone TEXT,
+                email TEXT,
+                sexo TEXT CHECK (sexo IN ('M', 'F'))
         );
         """)
 
     conexao.commit()
     cursor.close()
     conexao.close()
-    
-    
-    ### Cadastro de clientes ###
-    def cadastrar_cliente(nome,idade,telefone,email,sexo):
-        conexao = conectar()
-        cursor = conexao.cursor()
-        
-        cursor.execute("""
-                       INSERT INTO clientes (nome, idade, telefone, email, sexo) 
-                    VALUES (?, ?, ?, ?, ?)""",
-                    (nome,idade,telefone,email,sexo))
-        
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-        
-        
