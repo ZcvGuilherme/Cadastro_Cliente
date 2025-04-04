4aa07def65cf6d348eb67b5f2a9fda2016009ae5
Gabriel Carvalho <gabriellcartads2024@gmail.com>
2025-04-04 13:38:05 -0300

    Update models.py

---
+++ app/models.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+### CONEXÃO COM O BANCO DE DADOS ###
+
+def conectar():
+    conexao = sqlite3.connect("cadastro_clientes.db")
+    return conexao
+
+
+def criar_tabela_clientes():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+
+
+    cursor.execute("""
+               CREATE TABLE IF NOT EXISTS clientes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    nome TEXT  NOT NULL, 
+                    idade INTEGER NOT NULL,
+                    telefone TEXT,
+                    email TEXT,
+                    sexo TEXT (1) CHECK (sexo IN ('M','F'))
+        );
+        """)
+
+    conexao.commit()
+    conexao.close()
