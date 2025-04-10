d78bd7658a74ede20860b1bf47059779ae7ffd1c
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-04-10 21:17:43 +0000

    busca e ordenação completos

--- app/models.py
+++ app/models.py
@@ -56,13 +56,46 @@ def deletar_cliente(id):
         cursor.execute("DELETE FROM clientes WHERE id = ?",(id,))
         cursor.close()
         conexao.close()
-
-
 #buscar por nome
+def buscar_nome(nome):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    
+    cursor.execute("SELECT * FROM clientes WHERE nome LIKE ?", ('%' + nome+ '%',))
+    clientes = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return clientes
+    
 #buscar pelo email
+def buscar_email(email):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    
+    cursor.execute("SELECT * FROM clientes WHERE email LIKE ?", ('%' + email + '%',))
+    clientes = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return clientes
 
-#ordenar por :
-# id
-# nome 
-# idade
-# sexo
+def buscar_clientes_ordenado(campo='id', busca='', ordem=True):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    direcao = 'ASC' if ordem else 'DESC'
+    
+    # Previne injeção SQL ao validar o campo manualmente
+    campos_validos = ['id', 'nome', 'idade', 'sexo', 'email']
+    if campo not in campos_validos:
+        campo = 'id'
+
+    query = f"""
+        SELECT * FROM clientes 
+        WHERE nome LIKE ? OR email LIKE ? 
+        ORDER BY {campo} {direcao}
+    """
+    
+    cursor.execute(query, (f'%{busca}%', f'%{busca}%'))
+    clientes = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return clientes
