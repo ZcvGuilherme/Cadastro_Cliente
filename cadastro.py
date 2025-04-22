f592845c5862d9e12e0267a2c3522a80aa9d7397
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-04-22 14:52:43 -0300

    testes

--- cadastro.py
+++ cadastro.py
@@ -7,11 +7,33 @@ def go_and_click(x, y):
     pg.moveTo(x, y)
     pg.click()
 
+def cadastrar_produtos():
+    pg.PAUSE = 0.7
+    produtos = pd.read_excel("produtos2.xlsx")
+    time.sleep(3)
+
+    for index, row in produtos.iterrows():
+        nome = row['Nome']
+        quantidade = row['Quantidade']
+        valor = row['Valor']
+
+        time.sleep(2)
+        go_and_click(715, 328)
+        pg.write(nome, interval=0.03)
+
+        go_and_click(711, 381)
+        pg.write(str(quantidade), interval=0.03)
+
+        go_and_click(716, 427)
+        pg.write(str(valor), interval=0.03)
+
+        go_and_click(699, 477)
+
 
 def cadastrar_clientes():
     pg.PAUSE = 0.7
     time.sleep(2)
-    clientes = pd.read_excel("clientes.xlsx")
+    clientes = pd.read_excel("clientes2.xlsx")
     for index, row in clientes.iterrows():
         nome = row['Nome']
         email = row['Email']
@@ -20,16 +42,16 @@ def cadastrar_clientes():
         sexo = row['Sexo']
 
         go_and_click(712, 276)
-        pg.write(nome)
+        pg.write(nome, interval=0.03)
 
         go_and_click(714, 327)
-        pg.write(email)
+        pg.write(email, interval=0.03)
 
         go_and_click(721, 386)
-        pg.write(telefone)
+        pg.write(telefone, interval=0.03)
 
         go_and_click(657, 451)
-        pg.write(data_nasc.strftime("%d/%m/%Y"))  # Convertendo a data para o formato string
+        pg.write(data_nasc.strftime("%d/%m/%Y"), interval=0.03)  # Convertendo a data para o formato string
 
         go_and_click(717, 499)
         if sexo == "Masculino":
@@ -41,4 +63,4 @@ def cadastrar_clientes():
         time.sleep(0.5)  
 
 time.sleep(3)
-cadastrar_clientes()
+cadastrar_produtos()
